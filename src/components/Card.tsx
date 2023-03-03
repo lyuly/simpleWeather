12a534af29ff3ae071fec3ab3b3a93e1472a412f
lyuly <liumike567@gmail.com>
2023-03-03 10:43:03 +0800

    update layout

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -27,30 +27,51 @@ function Card () {
   }, [])
 
   return (
-    <div className='inline-flex justify-center items-start flex-col max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700'>
-      <h5 className='font-light text-4xl from-indigo-600 to-orange-300 text-transparent bg-clip-text bg-gradient-to-r'>
+    <div className='grid grid-cols-2 gap-4 justify-around max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+      <h5 className='font-semibold text-6xl from-indigo-400 to-blue-500 text-transparent bg-clip-text bg-gradient-to-r'>
         {city}
+        <div className='font-light text-2xl text-gray-400 text-center'>
+          {data != null ? data.weather[0].description : ''}
+        </div>
       </h5>
-      <p className='font-light text-2xl from-yellow-400 to-indigo-600 text-transparent bg-clip-text bg-gradient-to-r'>
-        {(data != null) ? data.main.temp : ''}
+
+      <img
+        className='rounded-full fill-current'
+        src={
+          './icons/' + (data != null ? data.weather[0].icon : '') +
+          '.png'
+        }
+      />
+      <p className='font-light text-6xl from-yellow-400 to-blue-600 text-transparent bg-clip-text bg-gradient-to-r'>
+        {data != null ? data.main.temp : ''}
         <span className='align-super text-base'>℃</span>
-        <span className='align-sub text-base'>{(data != null) ? data.main.humidity : ''}%</span>
       </p>
-      <p className='inline font-light from-blue-400 to-indigo-600 text-transparent bg-clip-text bg-gradient-to-r'>
-        <span>
-          <img className='rounded-full max-w-lg' src={'https://openweathermap.org/img/wn/' + ((data != null) ? data.weather[0].icon : '') + '@2x.png'} />
-          {(data != null) ? data.weather[0].description : ''}
+      <div className='font-light text-gray-400'>
+        <div className='flex justify-between'>
+        <span>体感</span>
+          <span className='font-light from-gray-400 to-rose-200 text-transparent bg-clip-text bg-gradient-to-l'>
+            {data != null ? data.main.feels_like : ''}℃
+          </span>
+        </div>
+        <div className='flex justify-between'>
+          <span>风速</span>
+          <span className='font-light from-gray-400 to-rose-200 text-transparent bg-clip-text bg-gradient-to-l'>
+            {data != null ? data.wind.speed : ''}m/s
+          </span>
+        </div>
+        <div className='flex justify-between'>
+          <span>湿度</span>
+          <span className='font-light from-gray-400 to-rose-200 text-transparent bg-clip-text bg-gradient-to-l'>
+          {data != null ? data.main.humidity : ''}%
         </span>
-      </p>
-      <p className='font-light from-gray-400 to-rose-200 text-transparent bg-clip-text bg-gradient-to-r'>
-        {(data != null) ? data.wind.deg : ''} {(data != null) ? (data.wind.speed) : ''}m/s
-      </p>
-      <p className='font-light from-lime-400 to-rose-200 text-transparent bg-clip-text bg-gradient-to-r'>
-        {(data != null) ? data.main.grnd_level : ''}pa
-      </p>
-      <p className='font-ligth from-gray-300 to-green-300 text-transparent bg-clip-text bg-gradient-to-l'>
-        日出{(data != null) ? dayjs(data.sys.sunrise).format('h:mm:ss') : ''}~日落{(data != null) ? dayjs(data.sys.sunset).format('HH:mm:ss') : ''}
-      </p>
+        </div>
+        <div className='flex justify-between'>
+          <span>大气压</span>
+          <span className='font-light from-gray-400 to-rose-200 text-transparent bg-clip-text bg-gradient-to-l'>
+            {data != null ? data.main.pressure : ''}pa
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
